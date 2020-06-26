8a1d24f705fda472c63f3bcf69fa5fa8912bfd04
Muhammad Faizan Qureshi <qfaizan401@gmail.com>
2020-06-27 00:30:55 +0500

    UI completed

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Header,Balance,IncomeExpense,TransectionList } from "./components";
+import { Header,Balance,IncomeExpense,TransectionList,AddTransection } from "./components";
 import './App.css';
 
 function App() {
@@ -10,6 +10,7 @@ function App() {
         <Balance />
         <IncomeExpense />
         <TransectionList />
+        <AddTransection />
       </div>
     </div>
   );
--- src/components/IncomeExpense.js
+++ src/components/IncomeExpense.js
@@ -5,11 +5,11 @@ const IncomeExpense = () => {
         <div class="inc-exp-container">
             <div>
                 <h4>Income</h4>
-                <p id="money-plus" class="money plus">+$0.00</p>
+                <p id="money-plus" class="money plus">+Rs: 0.00/=</p>
             </div>
             <div>
                 <h4>Expense</h4>
-                <p id="money-minus" class="money minus">-$0.00</p>
+                <p id="money-minus" class="money minus">-Rs: 0.00/=</p>
             </div>
       </div>
     )
