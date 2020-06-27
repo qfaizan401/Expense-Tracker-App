2fca973fa7f45868d0dd01b5f48d6dc77ceca5c0
Muhammad Faizan Qureshi <qfaizan401@gmail.com>
2020-06-28 03:49:30 +0500

    Update AddTransection.js

--- src/components/AddTransection.js
+++ src/components/AddTransection.js
@@ -1,17 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const AddTransection = () => {
+    const [Description, setDescription] = useState()
+    const [Amount, setAmount] = useState()
     return(
         <div>
             <h3>Add new transaction</h3>
             <form>
                 <div className="form-control">
                     <label htmlFor="text">Text</label>
-                    <input type="text" placeholder="Enter text..." />
+                    <input type="text" 
+                        placeholder="Enter text..."  
+                        value={Description} 
+                        onChange={ (event) => setDescription (event.target.value) }/>
                 </div>
                 <div className="form-control">
                     <label htmlFor="amount">Amount<br />(negative - expense, positive - income)</label>
-                    <input type="number" placeholder="Enter amount..." />
+                    <input type="number" 
+                    placeholder="Enter amount..." 
+                    value={Amount}
+                    onChange={ (event) => setAmount (event.target.value) }/>
                 </div>
                 <button className="btn">Add transaction</button>
             </form>
