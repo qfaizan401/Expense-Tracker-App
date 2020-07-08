54fb4ccd283dc17fe4606479c02d19f070949f57
Muhammad Faizan Qureshi <qfaizan401@gmail.com>
2020-07-08 11:40:44 +0500

    Update DescriptionAmount.js

--- src/context/DescriptionAmount.js
+++ src/context/DescriptionAmount.js
@@ -1,2 +1,12 @@
-import React from 'react'
+import React, { createContext } from 'react'
 
+const initialState = {
+    transection: [
+        { id: 1, text: 'Flower', amount: -20 },
+        { id: 2, text: 'Salary', amount: 300 },
+        { id: 3, text: 'Book', amount: -10 },
+        { id: 4, text: 'Camera', amount: 150 }
+    ]
+}
+
+export const DescriptionAmountContext = createContext(initialState)
