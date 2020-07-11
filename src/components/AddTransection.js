95ad2fe8c4ec03c1a7ff39f45d574d774fb36d43
Muhammad Faizan Qureshi <qfaizan401@gmail.com>
2020-07-11 17:30:36 +0500

    Update AddTransection.js

--- src/components/AddTransection.js
+++ src/components/AddTransection.js
@@ -8,7 +8,7 @@ const AddTransection = () => {
     const onSubmit = (event) => {
         event.preventDefault()
         const newTransection = {
-            id: Math.floor(Math.random() * 1000000),
+            id: Math.floor(Math.random() * 100000000),
             Description,
             Amount: +Amount
         }
