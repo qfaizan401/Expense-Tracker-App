998e50d4ea52eef4000d1bd0309100c6d6c526a5
Muhammad Faizan Qureshi <qfaizan401@gmail.com>
2020-07-13 03:29:21 +0500

    Delete all App

--- src/components/index.js
+++
@@ -1,6 +0,0 @@
-export { default as Header } from './Header'
-export { default as Balance } from './Balance'
-export { default as IncomeExpense } from './IncomeExpense'
-export { default as TransectionList } from './TransectionList'
-export { default as AddTransection } from './AddTransection'
-export { default as Transection } from './Transection'
