31716a09bac8c987f91a93525177e2eb6406683d
Muhammad Faizan Qureshi <qfaizan401@gmail.com>
2020-07-13 04:20:42 +0500

    Remove class and id

--- src/components/AddTransactions.js
+++ src/components/AddTransactions.js
@@ -4,16 +4,16 @@ export const AddTransactions = () => {
     return (
         <div>
             <h3>Add new transaction</h3>
-            <form id="form">
-                <div class="form-control">
-                    <label for="text">Text</label>
+            <form>
+                <div className="form-control">
+                    <label htmlFor="text">Text</label>
                     <input type="text" id="text" placeholder="Enter text..." />
                 </div>
-                <div class="form-control">
-                    <label for="amount">Amount <br /> (negative - expense, positive - income)</label>
+                <div className="form-control">
+                    <label htmlFor="amount">Amount <br /> (negative - expense, positive - income)</label>
                     <input type="number" id="amount" placeholder="Enter amount..." />
                 </div>
-                <button class="btn">Add transaction</button>
+                <button className="btn">Add transaction</button>
             </form>
         </div>
     )
